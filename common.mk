1026ecd96da99528ffa1c66128562b7d1ba760c5
Dauren <thedauke@gmail.com>
2020-06-24 21:10:49 +0000

    force overlay

--- common.mk
+++ common.mk
@@ -9,9 +9,8 @@ $(call inherit-product, vendor/meizu/sdm710-common/sdm710-common-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-device \
-    $(LOCAL_PATH)/overlay-sdm710 \
-    $(LOCAL_PATH)/overlay-lineage 
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-sdm710 
 
 # Product_Package_Overlay 
  PRODUCT_PACKAGE_OVERLAYS +=  $(LOCAL_PATH)/overlay-product
